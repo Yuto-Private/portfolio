ad99e8ef6de2a25d87949126b5dc67fd9bed04fe
YutoTakahashi <takahashi.development@gmail.com>
2019-08-03 19:32:12 +0900

    #vcdv6 カードコンポーネント作成

---
+++ components/Card.vue
@@ -0,0 +1,75 @@
+<template>
+  <nuxt-link tag='li' :to="'detail/'+slag+'/'" class="card">
+    <div class='card_thumbnail'>
+      <img :src='thumb' alt=''>
+    </div>
+    <div class='card_text'>
+      <p>{{ title }}</p>
+      <p>{{launch}}</p>
+    </div>
+  </nuxt-link>
+</template>
+
+<style scoped lang='scss'>
+  .card {
+
+    width: 30%;
+    cursor: pointer;
+
+    &_thumbnail {
+      position: relative;
+      transition: transform 0.5s cubic-bezier(0.19, 1, 0.22, 1);
+      img {
+        position: relative;
+        z-index: 2;
+      }
+      &:after {
+        content: '';
+        display: block;
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        transition: transform 0.5s cubic-bezier(0.19, 1, 0.22, 1);
+        transform: translateX(0) translateY(0);
+        background-image: linear-gradient(90deg, rgba(204,102,153,1) 35%, rgba(237,169,197,1) 100%);
+      }
+    }
+
+    &_text {
+      margin-top: 25px;
+      transition: transform 0.5s cubic-bezier(0.19, 1, 0.22, 1);
+      p {
+        &:first-child {
+          font-size: 20px;
+          letter-spacing: 1.5px;
+        }
+        &:last-child {
+          font-size: 12px;
+          margin-top: 10px;
+        }
+      }
+    }
+
+    &:hover {
+      .card_thumbnail {
+        transform: rotateX(30deg) rotateY(-20deg) rotateZ(5deg);
+        &:after {
+          transform: translateX(20px) translateY(20px);
+        }
+      }
+      .card_text {
+        color: $mainColor;
+      }
+    }
+
+  }
+</style>
+
+
+<script>
+export default {
+  props: ['title','slag','thumb','launch']
+}
+</script>
---
+++ components/CardList.vue
@@ -0,0 +1,36 @@
+<template>
+  <ul class="cardList">
+    <Card
+      v-for="post in posts"
+      :key="posts"
+      :title="post.fields.title"
+      :slag="post.fields.slag"
+      :thumb="post.fields.keyVisual.fields.file.url"
+      :launch="post.fields.launch"
+    />
+  </ul>
+</template>
+
+<style scoped lang='scss'>
+  .cardList {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+
+    li {
+      &:nth-child(n + 4) {
+        margin-top: 70px;
+      }
+    }
+  }
+</style>
+
+
+<script>
+import Card from '@/components/Card'
+
+export default {
+  components: { Card },
+  props: ['posts']
+}
+</script>
