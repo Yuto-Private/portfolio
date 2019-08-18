67a2f107576cc186196e48ae2eedf7f7214c7ae2
YutoTakahashi <takahashi.development@gmail.com>
2019-08-18 14:03:57 +0900

    #vcdv6 featureエリアのコンポーネントka

--- components/Feature.vue
+++ components/Feature.vue
@@ -1,23 +1,80 @@
 <template>
-  <section class="feature">
-    <h3><span>PORTFOLIO</span></h3>
+
+  <section class="feature rise" :class="{reversal:reversal}">
+    <h3 class='feature_title' :style="titlePosition"><span>{{title}}</span></h3>
     <div class='feature_contents'>
-      <CardList :posts="posts" />
+      <slot />
     </div>
-    <div class='feature_texture'></div>
+    <div class='feature_texture' />
   </section>
+
 </template>
 
-<style lang="scss">
-
-
-</style>
-
 <script>
-import Card from '@/components/Card'
 
 export default {
-  components: { Card },
-  props: ['posts']
+
+  props: ['title','reversal','titlePositionAdjust'],
+
+  data() {
+    return {
+      titlePosition: {
+        transform: 'rotate(90deg) translate('+ this.titlePositionAdjust +')'
+      }
+    }
+  }
+
 }
 </script>
+
+<style lang='scss'>
+
+  .feature {
+    position: relative;
+    padding: 50px 0;
+    margin-top: 200px;
+
+    &.reversal {
+      padding: 50px 0 110px;
+    }
+
+    &_title {
+      font-family: 'FujillaOne',sans-serif;
+      position: absolute;
+      font-size: 40px;
+      letter-spacing: 10px;
+      top: 0;
+      right: 0;
+      z-index: 10;
+      .reversal & {
+        right: auto;
+        left: 0;
+      }
+    }
+
+    &_contents {
+      position: relative;
+      width: 90%;
+      z-index: 2;
+      .reversal & {
+        margin-left: auto;
+      }
+    }
+
+    &_texture {
+      position: absolute;
+      width: 80%;
+      height: 100%;
+      right: 0;
+      top: 0;
+      background-color: $mainColor;
+      opacity: .5;
+      .reversal & {
+        right: auto;
+        left: 0;
+        background-color: $subColor;
+      }
+    }
+  }
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,51 +3,41 @@
 
     <TopTextAnimetion />
 
-    <section class="feature rise">
-      <h3 class='feature_title'><span>PORTFOLIO</span></h3>
-      <div class='feature_contents'>
-        <CardList :posts="posts" />
-      </div>
-      <div class='feature_texture'>
-      </div>
-    </section>
+    <Feature :title="'PORTFOLIO'" :titlePositionAdjust="'62%, -200%'">
+      <CardList :posts="posts" />
+    </Feature>
 
-    <section class="feature reversal rise">
-      <h3 class='feature_title'><span>PROFILE</span></h3>
-      <div class='feature_contents'>
-        <div class='profile'>
-          <Glitch 
-            :className="'profile_image'" 
-            :width="'40%'" 
-            :height="'40vw'"
-            :minHeight="'550px'" 
-            :path="'/images/about.jpg'" 
-          />
-          <div class='profile_detail'>
-            <div class='profile_detail_inner'>
-              <h4>Yuto Takahashi</h4>
-              <dl class="profile_detail_item">
-                <dt>SKILL SET</dt>
-                <dd>
-                  <img v-for='item in items' :src="'/icon/'+item+'.svg'">
-                </dd>
-              </dl>
-              <dl class="profile_detail_item">
-                <dt>BIOGRAPHY</dt>
-                <dd>
-                  adidas・reebok ECサイト運用チームのリーダー・リードエンジニアとして業務に従事しております。
-                  業務では主にReactやadidasチーム独自のテンプレートエンジンを用いたLP用のモジュール作成や、
-                  アニメーションの多いランディングページの実装と技術的なディレクション、チームビルディングなどを担当し、
-                  副業ではLPやwordpressサイトの作成や更新なども引き受け、個人開発ではvueやnuxtの勉強中です。
-                </dd>
-              </dl>
-            </div>
+    <Feature :title="'PROFILE'" :reversal='true' :titlePositionAdjust="'62%, 130%'">
+      <div class='profile'>
+        <Glitch 
+          :className="'profile_image'" 
+          :width="'40%'" 
+          :height="'40vw'"
+          :minHeight="'550px'" 
+          :path="'/images/about.jpg'" 
+        />
+        <div class='profile_detail'>
+          <div class='profile_detail_inner'>
+            <h4>Yuto Takahashi</h4>
+            <dl class="profile_detail_item">
+              <dt>SKILL SET</dt>
+              <dd>
+                <img v-for='item in items' :key='items' :src="'/icon/'+item+'.svg'">
+              </dd>
+            </dl>
+            <dl class="profile_detail_item">
+              <dt>BIOGRAPHY</dt>
+              <dd>
+                adidas・reebok ECサイト運用チームのリーダー・リードエンジニアとして業務に従事しております。
+                業務では主にReactやadidasチーム独自のテンプレートエンジンを用いたLP用のモジュール作成や、
+                アニメーションの多いランディングページの実装と技術的なディレクション、チームビルディングなどを担当し、
+                副業ではLPやwordpressサイトの作成や更新なども引き受け、個人開発ではvueやnuxtの勉強中です。
+              </dd>
+            </dl>
           </div>
         </div>
       </div>
-      <div class='feature_texture'>
-      </div>
-    </section>
+    </Feature>
 
   </div>
 </template>
@@ -59,56 +49,6 @@
     transition: opacity .5s cubic-bezier(.17,.67,.65,.54);
     &.active {
       opacity: 1;
-    }
-  }
-
-  .feature {
-    position: relative;
-    padding: 50px 0;
-    margin-top: 200px;
-
-    &.reversal {
-      padding: 50px 0 110px;
-    }
-
-    &_title {
-      font-family: 'FujillaOne',sans-serif;
-      position: absolute;
-      font-size: 40px;
-      letter-spacing: 10px;
-      top: 0;
-      right: 0;
-      transform: rotate(90deg) translate(62%, -200%);
-      z-index: 10;
-      .reversal & {
-        right: auto;
-        left: 0;
-        transform: rotate(90deg) translate(62%, 130%);
-      }
-    }
-
-    &_contents {
-      position: relative;
-      width: 90%;
-      z-index: 2;
-      .reversal & {
-        margin-left: auto;
-      }
-    }
-
-    &_texture {
-      position: absolute;
-      width: 80%;
-      height: 100%;
-      right: 0;
-      top: 0;
-      background-color: $mainColor;
-      opacity: .5;
-      .reversal & {
-        right: auto;
-        left: 0;
-        background-color: $subColor;
-      }
     }
   }
 
@@ -169,6 +109,7 @@
   import { TweenMax } from 'gsap'
   import { client } from '~/plugins/contentful.js'
   import TopTextAnimetion from '@/components/TopTextAnimetion'
+  import Feature from '@/components/Feature'
   import CardList from '@/components/CardList'
   import Glitch from '@/components/Glitch'
 
@@ -177,7 +118,7 @@
       return { items: [ 'html5','css3','javascript','php','git','wordpress','nuxt','react' ] }
     },
     components: {
-      CardList,TopTextAnimetion,Glitch
+      TopTextAnimetion,Feature,CardList,Glitch
     },
     asyncData(context) {
       return client.getEntries({
