baa885db91a3aced8403cb72af6d523a9fe5908d
YutoTakahashi <takahashi.development@gmail.com>
2019-08-25 02:57:16 +0900

    #vcdv6 エラーページspレイアウト調整

--- layouts/error.vue
+++ layouts/error.vue
@@ -7,17 +7,14 @@
         既に削除されているか、移動された可能性があります。<br>
         お手数ですが、下記リンクからサイトTOPへ移動してください。
       </p>
-      <nuxt-link to="/" tag='div' class='button mod-scale'>
-        <span>BACK TO TOP</span>
-      </nuxt-link>
     </div>
     <div class='errorPage_other' v-else>
       <h1 class="textGlitch" data-text="ERROR">ERROR</h1>
       <h2>エラーが起きました</h2>
-      <nuxt-link to="/" tag='div' class='button mod-scale'>
-        <span>BACK TO TOP</span>
-      </nuxt-link>
     </div>
+    <nuxt-link to="/" tag='div' class='button mod-scale'>
+      <span>BACK TO TOP</span>
+    </nuxt-link>
   </div>
 </template>
 
@@ -63,14 +60,20 @@ export default {
   text-align: center;
 
   h2 {
-    font-size: 23px;
+    font-size: 17px;
     color: #ab2328;
     margin-top: 20px;
+    @include mediaQuery(desktopFollow) {
+      font-size: 23px;
+    }
   }
   p {
-    font-size: 14px;
+    font-size: 11px;
     line-height: 1.45;
     margin-top: 20px;
+    @include mediaQuery(desktopFollow) {
+      font-size: 16px;
+    }
   }
 
   .button {
@@ -78,17 +81,28 @@ export default {
     margin: 30px auto 0;
   }
 
+  .textGlitch {
+    font-size: 40px;
+    @include mediaQuery(desktopFollow) {
+      font-size: 70px;
+    }
+  }
+
   &_404 {
     .textGlitch {
-      font-size: 100px;
-      width: 731px;
+      width: 294px;
+      @include mediaQuery(desktopFollow) {
+        width: 513px;
+      }
     }
   }
 
   &_other {
     .textGlitch {
-      font-size: 100px;
-      width: 351px;
+      width: 142px;
+      @include mediaQuery(desktopFollow) {
+        width: 247px;
+      }
     }
   }
 
